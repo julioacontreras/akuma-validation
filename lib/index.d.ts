11d91e008e83d83b7f4fd0bab6756d5f96bd9c0a
julioacontreras <julio.aranis.contreras@gmail.com>
2021-11-13 14:52:45 +0100

    first commit

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+export declare class Validator {
+    private validator;
+    isEmail(email: string): boolean;
+    isString(value: string): boolean;
+    has(data: any, key: string): boolean;
+}
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+const joi = require('joi');
+
+export class Validator {
+  private validator = joi 
+  isEmail (email: string): boolean {
+    if (!email) {
+      return false;
+    }
+    const schema = this.validator.string().email();
+    const result = schema.validate(email)
+    return result.error ? false : true 
+  }
+  isString (value: string): boolean {
+    const schema = this.validator.string();
+    const result = schema.validate(value)
+    return result.error ? false : true 
+  }
+  has (data: any, key: string): boolean {
+    return data[key]
+  }
+} 
